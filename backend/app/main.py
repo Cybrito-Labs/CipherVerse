310ea807f4ff189d63e1b8fb63f5281c6a200455
prashanthyadavv <prashanthyadavvvv@outlook.com>
2026-02-22 17:08:07 +0530

    classical tools

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-
+from app.core.classical import caesar_cipher
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "CipherVerse backend running"}
+
+
+
+print(caesar_cipher("HELLO", 3))
